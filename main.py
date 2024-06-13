db80819bfccea32a6ac176d7b31390c80517abf8
Andreas Marcec <a.marcec.extern@senec.com>
2024-06-13 14:53:57 +0200

    WIP cost estimation
    * Start refactor of transcibe functions
    * Estimate price of transcription based on length of video

--- main.py
+++ main.py
@@ -12,8 +12,14 @@ import yt_dlp as youtube_dl
 from pydub import AudioSegment
 import math
 
+import tiktoken
+
 load_dotenv()
 
+# Test: Transcribing https://www.youtube.com/watch?v=Wpf7G7E1Jzw
+#       and using Tiktoken with gpt-4o results into 14267 tokens
+#       so we have 14267 tokens / ~52min = ~ 275 token/min
+#       now we can use to base our estimation of the input price
 
 @click.group(chain=True)
 def cli() -> None:
@@ -24,15 +30,20 @@ OPENAI_API_KEY = os.getenv('OPENAI_KEY')
 logger = logging.getLogger(__name__)
 
 
-def split_mp3(file_path: str, chunk_size_mb: int) -> int:
+def get_num_tokens(very_long_string: str):
+    enc = tiktoken.encoding_for_model("gpt-4o")
+    return len(enc.encode(very_long_string))
+
+
+# TODO Whipser has a limit of 25MB per request -> Either find better way to split or split again if to large
+def split_mp3(file_path: str, chunk_len_in_s=1000) -> int:
     logger.info("Splitting the mp3")
     # Load the MP3 file
     audio = AudioSegment.from_mp3(file_path)
     title = file_path.split('.')[0]
 
     # Calculate the chunk size in milliseconds
-    chunk_size_ms = (chunk_size_mb * 1024 * 1024) / (audio.frame_rate * (audio.sample_width * 8) / 8.0 * audio.channels)
-    chunk_size_ms = math.floor(chunk_size_ms * 1000)  # Convert to milliseconds
+    chunk_size_ms = chunk_len_in_s * 1000  # Convert to milliseconds
 
     expected_chunks = math.ceil(len(audio) / chunk_size_ms)
     existing_chunks = [fname for fname in os.listdir('.') if f'{title}_' in fname and fname.endswith('mp3')]
@@ -43,7 +54,6 @@ def split_mp3(file_path: str, chunk_size_mb: int) -> int:
 
     # Split the audio into chunks
     chunks = [audio[i:i + chunk_size_ms] for i in range(0, len(audio), chunk_size_ms)]
-    print(chunks)
 
     # Export each chunk
     for i, chunk in enumerate(chunks):
@@ -54,7 +64,7 @@ def split_mp3(file_path: str, chunk_size_mb: int) -> int:
 
 
 def transcribe(client: OpenAI, audio_file: BinaryIO) -> str | None:
-    logger.info("Sending request to GPT for transcription")
+    logger.info(f"Transcribing {audio_file.name}")
     try:
         transcription = client.audio.transcriptions.create(
             model="whisper-1",
@@ -66,16 +76,15 @@ def transcribe(client: OpenAI, audio_file: BinaryIO) -> str | None:
         logger.error(f"OpenAI API error: {e}")
 
 
-def text_to_speech(client: OpenAI, text: str, output_filename: str) -> None:
-    logger.info("Sending text to create audio file")
-    try:
-        client.audio.speech.create(
-            model="tts-1",
-            voice="nova",
-            input=text
-        ).stream_to_file(output_filename)
-    except OpenAIError as e:
-        logger.error(f"OpenAI API error: {e}")
+def transcribe_chunks(client: OpenAI, num_of_chunks, title) -> str:
+    logger.info("Starting to transcribe all chunks")
+    combined_transcription = []
+
+    for i in range(num_of_chunks):
+        with open(f'{title}_{i}.mp3', "rb") as audio_file:
+            combined_transcription.append(transcribe(client, audio_file))
+
+    return " ".join(combined_transcription)
 
 
 def gpt_summarize(client: OpenAI, prompt: [dict[str, str]]) -> str | None:
@@ -90,6 +99,49 @@ def gpt_summarize(client: OpenAI, prompt: [dict[str, str]]) -> str | None:
         return response.choices[0].message.content.replace('\n', ' ').replace('\r', '')
     except OpenAIError as e:
         logger.error(f'OpenAI API error: {e}')
+
+
+def text_to_speech(client: OpenAI, text: str, output_filename: str) -> None:
+    logger.info("Sending text to create audio file")
+    try:
+        client.audio.speech.create(
+            model="tts-1",
+            voice="nova",
+            input=text
+        ).stream_to_file(output_filename)
+    except OpenAIError as e:
+        logger.error(f"OpenAI API error: {e}")
+
+
+def estimate_transcription(playtime: int):
+    print(playtime)
+    print(math.ceil(playtime/60))
+    print(float(os.getenv("WHISPER_PRICE")))
+    return math.ceil(playtime/60) * float(os.getenv("WHISPER_PRICE"))
+
+
+@click.command()
+@click.option('--url', required=True)
+def estimate_cost(url: str):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl': '%(title)s.%(ext)s',
+        'logger': logger,
+        'restrictfilenames': True
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        duration = info["duration"]
+        logger.info(f"Duration: {duration}")
+
+        print(estimate_transcription(duration))
+
 
 
 @click.command()
@@ -142,10 +194,13 @@ def summarize(log: str, url: str, clean: bool, tts: bool) -> None:
     else:
         logger.info("Skipping download, file found locally")
 
-    num_of_chunks = split_mp3(f'{title}.mp3', 200)
+    num_of_chunks = split_mp3(f'{title}.mp3')
+
+    client = OpenAI(api_key=OPENAI_API_KEY)
+    combined_transcription = transcribe_chunks(client, num_of_chunks, title)
+
 
     content = []
-    client = OpenAI(api_key=OPENAI_API_KEY)
 
     if not f'{title}.txt' in os.listdir():
         for c in range(num_of_chunks):
@@ -156,11 +211,14 @@ def summarize(log: str, url: str, clean: bool, tts: bool) -> None:
                     print(content)
                 continue
 
+            # TODO Refactor the following lines and use
+            #      transcribe_chunks(...) instead
             with open(f'{title}_{c}.mp3', "rb") as file:
                 if transcription := transcribe(client, file):
                     prompt = [
                         {"role": "system", "content": "You are a helpful assistant."},
-                        {"role": "user", "content": f"Summarize the following text into bullet points: {transcription}."},
+                        {"role": "user",
+                         "content": f"Summarize the following text into bullet points: {transcription}."},
                     ]
 
                     if summary := gpt_summarize(client, prompt):
@@ -194,5 +252,6 @@ def summarize(log: str, url: str, clean: bool, tts: bool) -> None:
 
 if __name__ == '__main__':
     cli.add_command(summarize)
+    cli.add_command(estimate_cost)
     cli()
-
+    # split_mp3("Life_on_House_and_Portraying_Dr._Taub_Peter_Jacobson-[MMIrp0F4_sE].mp3")
